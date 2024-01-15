1816401883f4e897013a7f54c46bc87e38077503
Peter Willis <31324861+peterwwillis@users.noreply.github.com>
2024-01-15 17:01:45 -0500

    Add colored logging output

--- palvella/lib/logging.py
+++ palvella/lib/logging.py
@@ -8,10 +8,22 @@ If environment variable 'DEBUG' is true, sets logging level to DEBUG.
 import logging
 import os
 
+from rich.console import Console
+from rich.logging import RichHandler
+
 DEBUG = os.environ.get('DEBUG', '0')
+COLOR_LOGGING = os.environ.get('COLOR_LOGGING', '1')
+COLOR_LOGGING_WIDTH = os.environ.get('COLOR_LOGGING_WIDTH', '160')
 
 def makeLogger(name):
-    logging.basicConfig()
+    colorlog = True if COLOR_LOGGING == "1" else False
+    colorlogwidth = int(COLOR_LOGGING_WIDTH, base=10)
+    console = Console(force_terminal=colorlog, width=colorlogwidth)
+    logging.basicConfig(
+            format="%(message)s",
+            datefmt="[%X]",
+            handlers=[RichHandler(rich_tracebacks=True, console=console)]
+    )
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     if DEBUG == "1":
